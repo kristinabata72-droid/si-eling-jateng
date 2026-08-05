79fe78e59e09141b4a5a2c9d855da3e466a08c63
kristinabata72 <kristinabata72@students.unnes.ac.id>
2026-08-05 10:20:02 +0700

    update tidb fallback password

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ export async function query(queryText: string, values: any[] = []) {
   // Ambil data koneksi dari .env (dengan nilai fallback TiDB Cloud)
   const host = process.env.DB_HOST || 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com';
   const user = process.env.DB_USER || '2b7GzwXu6GivvAE.root';
-  const password = process.env.DB_PASSWORD || 'a4cuC2p7gnpysrDD';
+  const password = process.env.DB_PASSWORD || 'Nkm3CbKxwup3iqzI';
   const database = (process.env.DB_NAME || process.env.DB_DATABASE || 'sieling').trim();
   const port = Number(process.env.DB_PORT) || 4000;
 
